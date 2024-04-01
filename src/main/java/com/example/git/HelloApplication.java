f90b101eb5f001c86e54a5c47905487993fa5399
Pavel <91691153+Pavel200421@users.noreply.github.com>
2024-04-01 21:02:18 +0700

    newCommit2

--- src/main/java/com/example/git/HelloApplication.java
+++ src/main/java/com/example/git/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, Habitat.getWidth(), Habitat.getHeight());
         stage.setScene(scene);
-        stage.setMaximized(false); // hahahah
+        stage.setMaximized(false); // hahahahaa
         stage.show();
     }
 
