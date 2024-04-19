137c4f0488ace10860c40f639ba84d488b7c77cc
Dizaled <silifslikirill@gmail.com>
2024-04-19 21:23:31 +0700

    Лаба 4: добавил картинки

--- src/main/java/com/example/git/HelloApplication.java
+++ src/main/java/com/example/git/HelloApplication.java
@@ -6,9 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
